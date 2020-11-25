089cf74000f238e56f93ee7be44406cf78f6d1b6
smaraujo <sanderson.mda@gmail.com>
2020-11-25 16:12:23 -0300

    incluir o projeto

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: 'ecf33e1e61f4c26276159a20df95f399',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiKey: 'ecf33e1e61f4c26276159a20df95f399',
+};
+
+
